ca97f6faabbdcd52a07f0214caa984cb47889bd1
Matthew Kimball <mekimba6@gmail.com>
2021-10-27 17:30:50 -0600

    adds another pet to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,5 @@
       shelter = Shelter.create!(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_1 = shelter.pets.create!(adoptable: true, age: 7, breed: 'sphynx', name: 'Fluffy')
       pet_2 = shelter.pets.create!(adoptable: true, age: 7, breed: 'tiger', name: 'Fluffy 2')
+      pet_3 = shelter.pets.create!(adoptable: true, age: 27, breed: 'boa constrictor', name: 'Carl')
       application = Application.create!(name: 'Jim Jimmerson', street: '123 Fake St', city: 'Denver', state: 'CO', zip_code: '90210')
